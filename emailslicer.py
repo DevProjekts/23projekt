c1ea3fd5e085e816424926f21234b91df1f48f07
DevProjekts <88111594+DevProjekts@users.noreply.github.com>
2021-09-16 17:27:38 +0200

    Create emailslicer.py

---
+++ emailslicer.py
@@ -0,0 +1,21 @@
+#Reading items from a list
+def append_lists(email): 
+    if '@' in email:
+        email_slicer = email.split('@')
+        user_name.append(email_slicer[0])
+        domain_name.append(email_slicer[1])
+    return user_name, domain_name
+
+if __name__ == '__main__':
+    #Delcaring variables
+    email_list = ['goku@dbz.com', 2, 3.14, 'vegeta@super.com', 'pq1', True]
+    user_name = []
+    domain_name = [] 
+    
+    #Loop through the list and apply function
+    for email in email_list:
+        try:
+            append_lists(email)
+        except TypeError:
+            continue
+    print(f'Usernames: {user_name} \nDomains: {domain_name}')
